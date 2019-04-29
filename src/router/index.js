be3969f4eeabcd4834a33ec59a9f7285efe0c5f0
dyyao <646617513@qq.com>
2019-04-29 16:55:05 +0800

    嵌套路由完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Login from '../views/Login';
-// import Layout from '../views/Layout';
-// import AllRecord from '../views/AllRecoed'; // 所有维修记录
-// import Sended from '../views/Sended'; // 所有维修记录
+import Layout from '../views/Layout';
+import AllRecord from '../views/AllRecoed'; // 所有维修记录
+import Sended from '../views/Sended'; // 已派单
 
 class Container extends React.Component {
     constructor(props) {
@@ -15,8 +15,17 @@ class Container extends React.Component {
         return (
             <Router>
                 <Switch>
-                    <Route path="/" component={Login}></Route>
-
+                    <Route path="/" component={Login} exact></Route>
+                    <Route path="/Layout" render={(props) => (
+                        <Layout {...props}>
+                            <Switch>
+                                <Route path="/Layout" exact render={() => (
+                                    <AllRecord {...props}  />
+                                )}></Route>
+                                <Route path="/Layout/sended" component={Sended}></Route>
+                            </Switch>
+                        </Layout>
+                    )}></Route>
                 </Switch>
             </Router>
         )
--- src/views/Layout/index.js
+++ src/views/Layout/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import {NavBar} from 'antd-mobile';
 
 class Layout extends React.Component {
     constructor(props) {
-        super(props)
-        this.state = {}
+        super(props);
+        const pathName = props.location.pathname;
+        let title = "";
+        switch (pathName) {
+            case "/Layout":
+                title = "所有维修记录";
+                break;
+            case "/Layout/sended":
+                title = "已派单";
+                break;
+            default:
+                title = "维修记录";
+        }
+        this.state = {
+            title
+        }
     }
 
     render() {
         return (
             <div className="layout-container">
+                <NavBar>{this.state.title}</NavBar>
                 {this.props.children}
             </div>
         )
+    }
+    componentDidMount () {
+        console.log(this.props)
     }
 }
 
